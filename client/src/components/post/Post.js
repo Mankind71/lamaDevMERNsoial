627c94164c14a1a18369d8332509cee5f329ef81
Mankind71 <makindetolulope37@gmail.com>
2022-02-05 18:46:14 +0000

    used axios to read data from api, deleted dummy data

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,9 +1,9 @@
 import { MoreVert } from "@material-ui/icons";
 import React, { useEffect, useState } from "react";
-import './post.css'; 
+import "./post.css";
 import axios from "axios";
 
-function Post({post}) {
+function Post({ post }) {
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
@@ -17,24 +17,18 @@ function Post({post}) {
     fetchUser();
   }, []);
 
-  const likeHandler = () =>{
-    setLike(isLiked ? like-1 : like+1)
-    setIsLiked(!isLiked)
-  }
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
 
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={user.profilePicture}
-              alt=""
-              className="postProfileImg"
-            />
-            <span className="postUsername">
-              {user.username}
-            </span>
+            <img src={user.profilePicture} alt="" className="postProfileImg" />
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
@@ -43,7 +37,7 @@ function Post({post}) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img src={PF+post.photo} alt="" className="postImg" />
+          <img src={PF + post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
